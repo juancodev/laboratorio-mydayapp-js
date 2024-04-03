5e438745a9a5d8de36069012c441abc1229b58eb
juancodev <montillasanchezjuancarlos@gmail.com>
2024-04-03 18:00:35 -0400

    [⚡ADD] Done functionality of remove the main and footer section - step 1

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,3 +1,19 @@
 export const sayHello = (text) => {
   return text;
 };
+
+const main = document.querySelector("#main");
+const footer = document.querySelector("#footer");
+
+document.addEventListener("DOMContentLoaded", () => {
+  main.style.display = "none";
+  footer.style.display = "none";
+});
+console.log(main);
+console.log(footer);
+
+// export const hiddenMainAndFooter = (mainSection, footerSection) => {
+//   if(mainSection && footerSection) {
+//     mainSection
+//   }
+// }
